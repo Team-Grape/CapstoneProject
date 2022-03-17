387104459ee3df5a3f1e3955cbf1f9560d3635aa
Chen Ying Xin <chenyingxin92@gmail.com>
2022-03-17 15:51:28 -0400

    added key

--- game.js
+++ game.js
@@ -9,6 +9,7 @@ kaboom({
 loadSprite("drawer", "drawer.png");
 loadSprite("background-tile", "basementTemplate.png");
 loadSprite("door", "evilDoor.png");
+loadSprite("key", "key_gold.png");
 
 onLoad(() => {
   add([sprite("background-tile"), scale(1), area()]);
@@ -21,4 +22,8 @@ onLoad(() => {
 
 onLoad(() => {
     add([sprite("door"), pos(500, 150), scale(4), area()]);
+  });
+
+  onLoad(() => {
+    add([sprite("key"), pos(90, 250), scale(1), area()]);
   });
