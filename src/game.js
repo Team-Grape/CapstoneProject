9d00fe4aae81a0964f60bd42a6c6377a0a921537
Mai <jea989@gmail.com>
2022-03-18 19:42:08 -0400

    fix-asset-links

--- src/game.js
+++ src/game.js
@@ -16,24 +16,24 @@ kaboom({
 // initializes or resets inventory in local storage
 window.localStorage.setItem("inventory", JSON.stringify([]));
 
-loadSprite('drawer', '../assets/drawer.png');
-loadSprite('background-tile', '../assets/basementTemplate.png');
-loadSprite('door', '../assets/evilDoor.png');
-loadSprite('key', '../assets/key_gold.png');
-loadSprite('room-two-background', '../assets/roomTwoBackground.png');
-loadSprite('room-two-background-right', '../assets/roomTwoRightLeft.png');
-loadSprite('room-two-background-left', '../assets/roomTwoRightLeft.png');
-loadSprite('right-arrow', '../assets/rightArrow.png');
-loadSprite('left-arrow', '../assets/leftArrow.png');
-loadSprite("up-arrow", "../assets/upArrow.png");
-loadSprite("down-arrow", "../assets/downArrow.png");
-loadSprite('fruit-painting', '../assets/fruitPainting.png');
+loadSprite('drawer', './assets/drawer.png');
+loadSprite('background-tile', './assets/basementTemplate.png');
+loadSprite('door', './assets/evilDoor.png');
+loadSprite('key', './assets/key_gold.png');
+loadSprite('room-two-background', './assets/roomTwoBackground.png');
+loadSprite('room-two-background-right', './assets/roomTwoRightLeft.png');
+loadSprite('room-two-background-left', './assets/roomTwoRightLeft.png');
+loadSprite('right-arrow', './assets/rightArrow.png');
+loadSprite('left-arrow', './assets/leftArrow.png');
+loadSprite("up-arrow", "./assets/upArrow.png");
+loadSprite("down-arrow", "./assets/downArrow.png");
+loadSprite('fruit-painting', './assets/fruitPainting.png');
 loadSprite('bookcase', './assets/bookcase.png');
-loadSprite('turn-right-arrow', '../assets/turn-right-arrow.png')
-loadSprite('turn-left-arrow', '../assets/turn-left-arrow.png')
-loadSprite('small-window', '../assets/small-window.png')
-loadSprite('pile-of-bones', '../assets/pileOfBones.png')
-loadSprite('chained-skeleton', '../assets/chainedSkeleton.png')
+loadSprite('turn-right-arrow', './assets/turn-right-arrow.png')
+loadSprite('turn-left-arrow', './assets/turn-left-arrow.png')
+loadSprite('small-window', './assets/small-window.png')
+loadSprite('pile-of-bones', './assets/pileOfBones.png')
+loadSprite('chained-skeleton', './assets/chainedSkeleton.png')
 
 
 // initialize components
