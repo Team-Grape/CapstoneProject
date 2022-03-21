773b11eb0a90b655d1d7f2d52b063792b7ead31f
Qina Zeng <qxqinazeng@gmail.com>
2022-03-21 17:57:04 -0400

    display all past message

--- src/core.js
+++ src/core.js
@@ -1,9 +1,9 @@
 export const checkInventoryForItem = (item) => {
-  if (!window.localStorage.getItem("inventory")) {
+  if (!window.localStorage.getItem('inventory')) {
     // you dont even have an inventory yet!
     return false;
   }
-  const currentInventory = JSON.parse(window.localStorage.getItem("inventory"));
+  const currentInventory = JSON.parse(window.localStorage.getItem('inventory'));
   if (currentInventory.filter((i) => i.name === item.name).length > 0) {
     // item is in the inventory!
     return true;
@@ -14,53 +14,89 @@ export const checkInventoryForItem = (item) => {
 };
 
 export const addToInventory = (item) => {
-  if (!window.localStorage.getItem("inventory")) {
-    window.localStorage.setItem("inventory", JSON.stringify([]));
+  if (!window.localStorage.getItem('inventory')) {
+    window.localStorage.setItem('inventory', JSON.stringify([]));
   }
-  let currentInventory = JSON.parse(window.localStorage.getItem("inventory"));
+  let currentInventory = JSON.parse(window.localStorage.getItem('inventory'));
   if (currentInventory.filter((i) => i.name === item.name).length === 0) {
     // this item is not in the inventory. So add it to the inventory below:
     localStorage.setItem(
-      "inventory",
+      'inventory',
       JSON.stringify([...currentInventory, item])
     );
   }
 };
 
+// ===================================================================================================
 
 export const addToMessageLog = (msg) => {
-  if (!window.localStorage.getItem("messageLog")) {
-    window.localStorage.setItem("messageLog", JSON.stringify([]));
+  if (!window.localStorage.getItem('messageLog')) {
+    window.localStorage.setItem('messageLog', JSON.stringify([]));
   }
-  let currentMessageLog = JSON.parse(window.localStorage.getItem("messageLog"));
-  localStorage.setItem("messageLog", JSON.stringify([...currentMessageLog, msg]))
-  
+  let currentMessageLog = JSON.parse(window.localStorage.getItem('messageLog'));
 
-}
+  currentMessageLog.push(msg);
 
+  localStorage.setItem('messageLog', JSON.stringify(currentMessageLog));
 
+  // const messag
+
+  const viewPastMessage = add([
+    text('Message Log', { size: 20 }),
+    pos(1050, 475),
+    area(),
+  ]);
+
+  viewPastMessage.onClick(() => {
+    const messageBox = add([
+      rect(width() / 2, height() - 200, { radius: 32 }),
+      origin('center'),
+      pos(center().x, center().y),
+    ]);
+
+    let msgY = height() / 2 - 160;
+    msg.forEach((currentMessage) => {
+      msgY = msgY + 20;
+      add([text(currentMessage, { size: 12 }), pos(width() / 2 - 280, msgY)]);
+    });
+
+    // const messages = add([
+    //   text('', { size: 16 }),
+    //   pos(width() / 2 - 280, height() / 2 - 160),
+    // ]);
+
+    const closeButton = add([text('X', { size: 30 }), pos(890, 400), area()]);
+
+    closeButton.onClick(() => {
+      messageBox.destroy();
+      closeButton.destroy();
+      messages.destroy();
+    });
+  });
+};
+
+// ==============================================================================================
 
 export const removeFromInventory = (item) => {
-  if (!window.localStorage.getItem("inventory")) {
-    window.localStorage.setItem("inventory", JSON.stringify([]));
+  if (!window.localStorage.getItem('inventory')) {
+    window.localStorage.setItem('inventory', JSON.stringify([]));
   }
 
-  let currentInventory = JSON.parse(window.localStorage.getItem("inventory"));
+  let currentInventory = JSON.parse(window.localStorage.getItem('inventory'));
 
   // this does not care about quantity! just removes if exists
   currentInventory = currentInventory.filter((i) => i.name != item.name);
-  localStorage.setItem("inventory", JSON.stringify(currentInventory));
-}
-
+  localStorage.setItem('inventory', JSON.stringify(currentInventory));
+};
 
 export const setGameState = (roomName, gameEvent, value) => {
-  if (!window.localStorage.getItem("gameState")) {
-    window.localStorage.setItem("gameState", JSON.stringify({}));
+  if (!window.localStorage.getItem('gameState')) {
+    window.localStorage.setItem('gameState', JSON.stringify({}));
   }
-  let currentGameState = JSON.parse(window.localStorage.getItem("gameState"));
+  let currentGameState = JSON.parse(window.localStorage.getItem('gameState'));
 
   // check if roomName has been created
-  if (Object.entries(currentGameState).some((room) => (room[0] == roomName))) {
+  if (Object.entries(currentGameState).some((room) => room[0] == roomName)) {
     // room object already exists, so we can add & set keys/values on it!
     currentGameState[roomName][gameEvent] = value;
   } else {
@@ -71,58 +107,58 @@ export const setGameState = (roomName, gameEvent, value) => {
   }
 
   // save the updated gameState to local storage
-  localStorage.setItem("gameState",  JSON.stringify(currentGameState));
+  localStorage.setItem('gameState', JSON.stringify(currentGameState));
 };
 
 export const getGameState = (roomName, gameEvent) => {
   // if gameState does not exist
-  if (!window.localStorage.getItem("gameState")) {
-    return false
-  } 
+  if (!window.localStorage.getItem('gameState')) {
+    return false;
+  }
 
-  let currentGameState = JSON.parse(window.localStorage.getItem("gameState"));
+  let currentGameState = JSON.parse(window.localStorage.getItem('gameState'));
 
   // if roomName does not exist
-  if (!Object.entries(currentGameState).some((room) => (room[0] == roomName))) {
-    return false
-  }  else {
+  if (!Object.entries(currentGameState).some((room) => room[0] == roomName)) {
+    return false;
+  } else {
     // roomName does exist; but if gameEvent doesnt exist, it will return undefined;
     // which evaluates to false
-    return currentGameState[roomName][gameEvent]
+    return currentGameState[roomName][gameEvent];
   }
-}
+};
 
 export function displayNavArrows(arrayOfDirectionsStrings = []) {
   for (let i = 0; i < arrayOfDirectionsStrings.length; i++) {
     let direction = arrayOfDirectionsStrings[i];
-    if (direction === "left") {
+    if (direction === 'left') {
       add([
-        sprite("left-arrow"),
+        sprite('left-arrow'),
         pos(7.5, 250),
         scale(1),
         area(),
-        "left-arrow",
+        'left-arrow',
       ]);
     }
-    if (direction === "right") {
+    if (direction === 'right') {
       add([
-        sprite("right-arrow"),
+        sprite('right-arrow'),
         pos(1190, 250),
         scale(1),
         area(),
-        "right-arrow",
+        'right-arrow',
       ]);
     }
-    if (direction === "up") {
-      add([sprite("up-arrow"), pos(600, 15), scale(1), area(), "up-arrow"]);
+    if (direction === 'up') {
+      add([sprite('up-arrow'), pos(600, 15), scale(1), area(), 'up-arrow']);
     }
-    if (direction === "down") {
+    if (direction === 'down') {
       add([
-        sprite("down-arrow"),
+        sprite('down-arrow'),
         pos(575, 475),
         scale(1),
         area(),
-        "down-arrow",
+        'down-arrow',
       ]);
     }
   }
@@ -130,89 +166,83 @@ export function displayNavArrows(arrayOfDirectionsStrings = []) {
 
 export const navArrows = (roomName) => (d) => {
   if (d == 'Up') {
-    onClick("right-arrow", () => {
-      go(roomName + "Right");
+    onClick('right-arrow', () => {
+      go(roomName + 'Right');
     });
-    onClick("left-arrow", () => {
-      go(roomName + "Left");
+    onClick('left-arrow', () => {
+      go(roomName + 'Left');
     });
-    onClick("down-arrow", () => {
-      go(roomName + "Down");
+    onClick('down-arrow', () => {
+      go(roomName + 'Down');
     });
-    displayNavArrows(["left", "right", "down"]);
+    displayNavArrows(['left', 'right', 'down']);
   } else if (d == 'Right') {
-    onClick("left-arrow", () => {
-      go(roomName + "Up");
+    onClick('left-arrow', () => {
+      go(roomName + 'Up');
     });
-    displayNavArrows(["left"]);
+    displayNavArrows(['left']);
   } else if (d == 'Down') {
-    onClick("up-arrow", () => {
-      go(roomName + "Up");
+    onClick('up-arrow', () => {
+      go(roomName + 'Up');
     });
-    displayNavArrows(["up"]);
+    displayNavArrows(['up']);
   } else if (d == 'Left') {
-    onClick("right-arrow", () => {
-      go(roomName + "Up");
+    onClick('right-arrow', () => {
+      go(roomName + 'Up');
     });
-    displayNavArrows(["right"]);
+    displayNavArrows(['right']);
   }
-}
-
+};
 
 export const textBubble = (dialogs, onFinish) => {
-    // Current dialog
-    let curDialog = 0;
+  // Current dialog
+  let curDialog = 0;
 
-    // Text bubble
-    const textbox = add([
-      rect(width() - 200, 120, { radius: 32 }),
-      origin("center"),
-      pos(center().x, height() - 100),
-      outline(2),
-    ]);
+  // Text bubble
+  const textbox = add([
+    rect(width() - 200, 120, { radius: 32 }),
+    origin('center'),
+    pos(center().x, height() - 100),
+    outline(2),
+  ]);
 
-    // Text
-    const txt = add([
-      text("", { size: 32, width: width() - 230 }),
-      pos(textbox.pos),
-      origin("center"),
-    ]);
+  // Text
+  const txt = add([
+    text('', { size: 32, width: width() - 230 }),
+    pos(textbox.pos),
+    origin('center'),
+  ]);
 
- //   console.log(txt)
+  //   console.log(txt)
 
-    // NextButton
-    const nextButton = add([
-      text("Next", { size: 20 }),
-      pos(1050, 475),
-      area(),
-    ]);
-    if (dialogs.length === 1) {
-      nextButton.text = "Close"
-    }
+  // NextButton
+  const nextButton = add([text('Next', { size: 20 }), pos(1050, 475), area()]);
+  if (dialogs.length === 1) {
+    nextButton.text = 'Close';
+  }
 
-    nextButton.onClick(() => {
+  nextButton.onClick(() => {
     /*  if (curDialog === dialogs.length - 2) {
         nextButton.text = "Close"
       } else */ if (curDialog === dialogs.length - 1) {
-        textbox.destroy();
-        txt.destroy();
-        nextButton.destroy();
-        curDialog = 0;
-        if (onFinish) {
-          onFinish()
-        }
-        return;
+      textbox.destroy();
+      txt.destroy();
+      nextButton.destroy();
+      curDialog = 0;
+      if (onFinish) {
+        onFinish();
       }
-      curDialog = curDialog + 1;
-      updateDialog();
-    });
-
-    // Update the on screen sprite & text
-    function updateDialog() {
-      const [dialog] = dialogs[curDialog];
-      txt.text = dialog;
+      return;
     }
-
+    curDialog = curDialog + 1;
     updateDialog();
+  });
 
-}
+  // Update the on screen sprite & text
+  function updateDialog() {
+    const [dialog] = dialogs[curDialog];
+    txt.text = dialog;
+  }
+
+  updateDialog();
+};
