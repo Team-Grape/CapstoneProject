37cd691845742df547b0712b1027035c957fe7c5
Chen Ying Xin <chenyingxin92@gmail.com>
2022-03-24 16:16:43 -0400

    added models

--- 3DRoom.js
+++ 3DRoom.js
@@ -10,22 +10,66 @@ const camera = new THREE.PerspectiveCamera(
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
-renderer.setClearColor(0xb7c3f3, 1);
-const light = new THREE.AmbientLight(0x404040); // soft white light
+
+
+const light = new THREE.AmbientLight(0xffffff); // soft white light
 scene.add(light);
 
-camera.position.z = 50
+const textureLoader = new THREE.TextureLoader();
+textureLoader.load(
+  "./dist/assets/room_backgrounds/room5Background.png",
+  function (texture) {
+    scene.background = texture;
+  }
+);
+
+camera.position.z = 70;
 
 const loader = new THREE.GLTFLoader();
 
-loader.load("./dist/assets/ghostModel/scene.gltf", (gltf) => {
+class Ghost {
+  constructor() {
+    loader.load("./dist/assets/ghostModel/scene.gltf", (gltf) => {
+      scene.add(gltf.scene);
+      gltf.scene.scale.set(0.1, 0.1, 0.1);
+      // gltf.scene.position.set(30, 20, 0);
+      this.ghost = gltf.scene;
+    });
+  }
+}
+loader.load("./dist/assets/gasStove/scene.gltf", (gltf) => {
   scene.add(gltf.scene);
-  gltf.scene.scale.set(0.1, 0.1, 0.1);
-  // gltf.scene.position.set(30, 20, 0);
-});
+  gltf.scene.scale.set(.3, .3, .3);
+  gltf.scene.position.set(-50, -20, -10);
+  gltf.scene.rotation.y = .55
+})
+loader.load("./dist/assets/kitchenCounter/scene.gltf", (gltf) => {
+  scene.add(gltf.scene);
+  gltf.scene.scale.set(20, 20, 20);
+  gltf.scene.position.set(50, -20, -10);
+  gltf.scene.rotation.y = -2
+})
+loader.load("./dist/assets/oldFridge/scene.gltf", (gltf) => {
+  scene.add(gltf.scene);
+  gltf.scene.scale.set(3, 3, 3);
+  gltf.scene.position.set(5, -20, -10);
+  // gltf.scene.rotation.y = 1.45
+})
+loader.load("./dist/assets/dirtyToilet/scene.gltf", (gltf) => {
+  scene.add(gltf.scene);
+  gltf.scene.scale.set(3, 3, 3);
+  gltf.scene.position.set(10, -20, -10);
+  gltf.scene.rotation.y = 1.45
+})
+
+const ghost = new Ghost();
 
 function animate() {
   renderer.render(scene, camera);
+  // ghost.position.y += 0.01;
+  // setTimeout(function(){
+  //   ghost.position.y += -0.02
+  // }, 1000);
   requestAnimationFrame(animate);
 }
 
