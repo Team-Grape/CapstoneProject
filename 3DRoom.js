727852ce71ca3669678859e0b02263b3db62b2ba
Qina Zeng <qxqinazeng@gmail.com>
2022-03-25 18:12:47 -0400

    add more 3d models

--- 3DRoom.js
+++ 3DRoom.js
@@ -17,9 +17,9 @@ document.body.appendChild(renderer.domElement);
 
 /*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> */
 //create controls for camera
-const controls = new THREE.OrbitControls( camera, renderer.domElement );
+const controls = new THREE.OrbitControls(camera, renderer.domElement);
 
-camera.position.set( 0, 20, 100 );
+camera.position.set(0, 20, 100);
 controls.update();
 
 /*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> */
@@ -28,12 +28,12 @@ const light = new THREE.AmbientLight(0xffffff); // soft white light
 scene.add(light);
 
 /*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> */
-//loading textures to planes/ walls 
-const textureLoader = new THREE.TextureLoader(); 
+//loading textures to planes/ walls
+const textureLoader = new THREE.TextureLoader();
 
 //loading first wall
 textureLoader.load(
-  "./dist/assets/room_backgrounds/brick.jpeg",
+  './dist/assets/room_backgrounds/brick.jpeg',
   function (texture) {
     texture.wrapS = THREE.RepeatWrapping;
     texture.wrapT = THREE.RepeatWrapping;
@@ -43,13 +43,13 @@ textureLoader.load(
     const material = new THREE.MeshLambertMaterial({ map: texture });
     const plane = new THREE.Mesh(geometry, material);
     scene.add(plane);
-    plane.position.z = -200
+    plane.position.z = -200;
   }
 );
 
 //loading second wall
 textureLoader.load(
-  "./dist/assets/room_backgrounds/brick.jpeg",
+  './dist/assets/room_backgrounds/brick.jpeg',
   function (texture) {
     texture.wrapS = THREE.RepeatWrapping;
     texture.wrapT = THREE.RepeatWrapping;
@@ -59,14 +59,14 @@ textureLoader.load(
     const material = new THREE.MeshLambertMaterial({ map: texture });
     const plane = new THREE.Mesh(geometry, material);
     scene.add(plane);
-    plane.rotation.y = (Math.PI / 2)
-    plane.position.x = -200
+    plane.rotation.y = Math.PI / 2;
+    plane.position.x = -200;
   }
 );
 
 // loading third wall
 textureLoader.load(
-  "./dist/assets/room_backgrounds/brick.jpeg",
+  './dist/assets/room_backgrounds/brick.jpeg',
   function (texture) {
     texture.wrapS = THREE.RepeatWrapping;
     texture.wrapT = THREE.RepeatWrapping;
@@ -76,14 +76,14 @@ textureLoader.load(
     const material = new THREE.MeshLambertMaterial({ map: texture });
     const plane = new THREE.Mesh(geometry, material);
     scene.add(plane);
-    plane.rotation.y = -(Math.PI / 2)
-    plane.position.x = 200
+    plane.rotation.y = -(Math.PI / 2);
+    plane.position.x = 200;
   }
 );
 
 // loading fourth wall
 textureLoader.load(
-  "./dist/assets/room_backgrounds/brick.jpeg",
+  './dist/assets/room_backgrounds/brick.jpeg',
   function (texture) {
     texture.wrapS = THREE.RepeatWrapping;
     texture.wrapT = THREE.RepeatWrapping;
@@ -93,15 +93,15 @@ textureLoader.load(
     const material = new THREE.MeshLambertMaterial({ map: texture });
     const plane = new THREE.Mesh(geometry, material);
     scene.add(plane);
-    plane.rotation.y = (Math.PI)
+    plane.rotation.y = Math.PI;
     // plane.position.x = 200
-    plane.position.z = 200
+    plane.position.z = 200;
   }
 );
 
-//loading floor 
+//loading floor
 textureLoader.load(
-  "./dist/assets/room_backgrounds/floor.jpeg",
+  './dist/assets/room_backgrounds/floor.jpeg',
   function (texture) {
     texture.wrapS = THREE.RepeatWrapping;
     texture.wrapT = THREE.RepeatWrapping;
@@ -112,8 +112,8 @@ textureLoader.load(
     const material = new THREE.MeshLambertMaterial({ map: texture });
     const plane = new THREE.Mesh(geometry, material);
     scene.add(plane);
-    plane.rotation.x = -1.57
-    plane.position.y = -112
+    plane.rotation.x = -1.57;
+    plane.position.y = -112;
   }
 );
 
@@ -126,42 +126,87 @@ const loader = new THREE.GLTFLoader();
 
 class Ghost {
   constructor() {
-    loader.load("./dist/assets/ghostModel/scene.gltf", (gltf) => {
+    loader.load('./dist/assets/3DModels/ghostModel/scene.gltf', (gltf) => {
       scene.add(gltf.scene);
-      const scale = .5
+      const scale = 0.5;
       gltf.scene.scale.set(scale, scale, scale);
       gltf.scene.position.set(30, 20, -200);
       this.ghost = gltf.scene;
     });
   }
 }
-loader.load("./dist/assets/gasStove/scene.gltf", (gltf) => {
+loader.load('./dist/assets/3DModels/gasStove/scene.gltf', (gltf) => {
   scene.add(gltf.scene);
   const scale = 0.7;
   gltf.scene.scale.set(scale, scale, scale);
   gltf.scene.position.set(-100, -110, -180);
 });
-loader.load("./dist/assets/kitchenCounter/scene.gltf", (gltf) => {
+loader.load('./dist/assets/3DModels/kitchenCounter/scene.gltf', (gltf) => {
   scene.add(gltf.scene);
   const scale = 50;
   gltf.scene.scale.set(scale, scale, scale);
   gltf.scene.position.set(10, -60, -180);
-  gltf.scene.rotation.y = -1.6
+  gltf.scene.rotation.y = -1.6;
 });
-loader.load("./dist/assets/oldFridge/scene.gltf", (gltf) => {
+loader.load('./dist/assets/3DModels/oldFridge/scene.gltf', (gltf) => {
   scene.add(gltf.scene);
   const scale = 70;
   gltf.scene.scale.set(scale, scale, scale);
   gltf.scene.position.set(80, -110, -150);
-  gltf.scene.rotation.y = (Math.PI)
+  gltf.scene.rotation.y = Math.PI;
 });
-loader.load("./dist/assets/purpleDoor/scene.gltf", (gltf) => {
+loader.load('./dist/assets/3DModels/purpleDoor/scene.gltf', (gltf) => {
   scene.add(gltf.scene);
-  const scale = .8;
+  const scale = 0.8;
   gltf.scene.scale.set(scale, scale, scale);
   // gltf.scene.rotation.y = (Math.PI / 2)
-  gltf.scene.position.set(-50, -30, 0);
+  gltf.scene.position.set(-196, -30, 0);
 });
+
+loader.load('./dist/assets/3DModels/kitchenCabinet/scene.gltf', (gltf) => {
+  scene.add(gltf.scene);
+  const scale = 0.08;
+  gltf.scene.scale.set(scale, scale, scale);
+  gltf.scene.position.set(117, -45, 160);
+  gltf.scene.rotation.y = -Math.PI / 2;
+});
+
+loader.load('./dist/assets/3DModels/kitchenCabinet/scene.gltf', (gltf) => {
+  scene.add(gltf.scene);
+  const scale = 0.08;
+  gltf.scene.scale.set(scale, scale, scale);
+  gltf.scene.position.set(-135, -45, 160);
+  gltf.scene.rotation.y = -Math.PI / 2;
+});
+
+loader.load('./dist/assets/3DModels/kitchenShelf/scene.gltf', (gltf) => {
+  scene.add(gltf.scene);
+  const scale = 0.08;
+  gltf.scene.scale.set(scale, scale, scale);
+  gltf.scene.position.set(-120, 40, 190);
+  gltf.scene.rotation.y = -Math.PI;
+});
+
+// could't see this one
+loader.load(
+  './dist/assets/3DModels/hangingKitchenCupboards/scene.gltf',
+  (gltf) => {
+    scene.add(gltf.scene);
+    const scale = 20;
+    gltf.scene.scale.set(scale, scale, scale);
+    gltf.scene.position.set(-120, 40, 160);
+    gltf.scene.rotation.y = -Math.PI;
+  }
+);
+
+loader.load('./dist/assets/3DModels/kitchenKnife/scene.gltf', (gltf) => {
+  scene.add(gltf.scene);
+  const scale = 0.2;
+  gltf.scene.scale.set(scale, scale, scale);
+  // gltf.scene.rotation.y = Math.PI / 2;
+  gltf.scene.position.set(100, -110, 20);
+});
+
 const ghost = new Ghost();
 
 function animate() {
@@ -180,6 +225,6 @@ function onWindowResize() {
   renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
-window.addEventListener("resize", onWindowResize, false);
+window.addEventListener('resize', onWindowResize, false);
 
 animate();
