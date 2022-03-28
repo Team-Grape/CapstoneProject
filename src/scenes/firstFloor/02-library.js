611564e87f83516d0bac0699ca0baf413bfee2e1
Violet Cutler <91086765+VioletCutler@users.noreply.github.com>
2022-03-28 16:15:44 -0400

    removed debugger

--- src/scenes/firstFloor/02-library.js
+++ src/scenes/firstFloor/02-library.js
@@ -68,7 +68,7 @@ export const createLibrary = () => {
 
   roomNavArrows(window.viewDirection)
 
-       debugRectSize();
+      //  debugRectSize();
   });
 
   // ======================================================== //
