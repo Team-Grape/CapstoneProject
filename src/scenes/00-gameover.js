6e0a65189ac77aaf098c90768c8c3d8a3d1fe2bb
Mai <jea989@gmail.com>
2022-03-30 15:18:38 -0400

    restart button added

--- src/scenes/00-gameover.js
+++ src/scenes/00-gameover.js
@@ -18,5 +18,16 @@ export const gameover = () => {
             pos(width() / 2, 400),
             origin("center"),
         ])
+        add([
+            text("click here to restart", {size: 16}),
+            color(255, 255, 255),
+            pos(width() / 2, 460),
+            origin("center"),
+            area(),
+            "restartButton"
+        ])
+        onClick("restartButton", () => {
+          go("title")
+        })
     })
 }
