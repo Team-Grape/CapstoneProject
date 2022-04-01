e76200a5012daa06c33fe9f5ad9d8407c53dfdc5
Chen Ying Xin <chenyingxin92@gmail.com>
2022-03-31 22:23:59 -0400

    added sound to evil ghosts

--- src/scenes/secondFloor/01-study.js
+++ src/scenes/secondFloor/01-study.js
@@ -21,24 +21,25 @@ export const createStudy = async () => {
 
     onLoad(() => {
       add([sprite("study"), scale(1)]);
-      add([sprite('heart-key'), pos(300, 300), scale(1), area(), 'heartKey'])
+      add([sprite("heart-key"), pos(300, 300), scale(1), area(), "heartKey"]);
     });
 
-    onClick('heartKey', (heartKey1) => {
-      if (!getGameState(roomName, 'heartKeyPickedUp')) {
-        playSFX('keyNoise');
-        textBubble([['A key was added to your inventory']], () => {
+    onClick("heartKey", (heartKey1) => {
+      if (!getGameState(roomName, "heartKeyPickedUp")) {
+        playSFX("keyNoise");
+        textBubble([["A key was added to your inventory"]], () => {
           singleViewNavArrow("studyDown", "secondFloorHallwayDown");
         });
         addToInventory(heartKey);
-        setGameState(roomName, 'silverKeyPickedUp', true);
+        setGameState(roomName, "silverKeyPickedUp", true);
         heartKey1.destroy();
       }
-    })
+    });
 
     const nunInterval = setInterval(() => {
       const nun = add([sprite("nun"), pos(330, 0), scale(1.6), area()]);
-      textBubble([["Boooooo!!"]])
+      textBubble([["Boooooo!!"]]);
+      playSFX("dundundun");
     }, 5000);
 
     const gameoverInterval = setInterval(() => {
@@ -46,12 +47,12 @@ export const createStudy = async () => {
     }, 7000);
 
     setTimeout(() => {
-        clearInterval(nunInterval);
-    }, 6000)
-        
+      clearInterval(nunInterval);
+    }, 6000);
+
     setTimeout(() => {
-        clearInterval(gameoverInterval)
-    }, 8000)
+      clearInterval(gameoverInterval);
+    }, 8000);
 
     singleViewNavArrow("studyDown", "secondFloorHallwayDown");
   });
